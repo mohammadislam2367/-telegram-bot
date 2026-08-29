4021a1dfe68b458876ab1238fa655ab5ce815eca
mohammadislam2367 <mohammadislam2367@gmail.com>
2026-08-29 13:46:23 +0430

    Create     bot.py

---
+++ bot.py
@@ -0,0 +1,30 @@
+import asyncio
+import nest_asyncio
+
+from telegram import Update
+from telegram.ext import Application, MessageHandler, filters, ContextTypes
+
+nest_asyncio.apply()
+
+TOKEN = "8787876825:AAGuifHjOrkUgS6UAmM2Rwfck9Tv-2euA_M"
+
+async def reply(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await update.message.reply_text("سلام 👋 پیام شما دریافت شد.")
+
+async def main():
+    app = Application.builder().token(TOKEN).build()
+
+    app.add_handler(
+        MessageHandler(filters.TEXT & ~filters.COMMAND, reply)
+    )
+
+    print("ربات روشن شد 🤖")
+
+    await app.initialize()
+    await app.start()
+    await app.updater.start_polling()
+
+    await asyncio.Event().wait()
+
+if __name__ == "__main__":
+    asyncio.run(main())
